c4235c51fd835e96330077b06c1e633441c794b4
Thomas Frick <thomas.m.frick@gmail.com>
2016-12-03 16:12:22 -0500

    finished game_logic and renamed it gridlogic
    includes a constructor, createGrid, swap, gridIsSolved, and getIndexOf
    method

--- gridlogic.py
+++ gridlogic.py
@@ -1,9 +1,11 @@
 import random
 
 #object for game logic so that it can be imported easily
-class Game_logic:
+class Grid_logic:
     #constructor
     def __init__(self, num_rows, num_cols, symbols, colors):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
         self.grid, self.answer_key = self.createGrid(num_rows, num_cols, symbols, colors)
         
 
@@ -22,9 +24,12 @@ class Game_logic:
             for col in range(0, num_cols):
                 duplicates.append(Symbol(color, symbols[col]))
         
+        # creates a duplicate of the sorted list (for answer key), and shuffles
+        #   the first list
         answer_key_prep = list(duplicates)
         random.shuffle(duplicates)
 
+        # creates a shuffled grid
         shuffled_grid = []
         for col in range(0, num_cols):
             col = []
@@ -32,6 +37,7 @@ class Game_logic:
                 col.append(duplicates.pop())
             shuffled_grid.append(col)
 
+        # creates an answer key
         answer_key = []
         for col in range(0, num_cols):
             col = []
@@ -39,23 +45,40 @@ class Game_logic:
                 col.append(answer_key_prep.pop())
             answer_key.append(col)
 
+        # returns the shuffled grid and answer key in a tuple
         return (shuffled_grid, answer_key)
 
-    def checkGrid(self):
-        pass
-                
-            
+    # returns true if the player's grid is the same as the answer key
+    def gridIsSolved(self):
+        return self.grid == self.answer_key
 
+    # swaps the location of two items on the grid            
     def swap(self, symbol_1, symbol_2):
-        pass
+        index_of_1 = self.getIndexOf(symbol_1)
+        index_of_2 = self.getIndexOf(symbol_2)
+        if index_of_1 != (-1, -1) and index_of_2 != (-1, -1):
+            col1, row1 = index_of_1
+            self.grid[col1].insert(row1, symbol_2)
+            self.grid[col1].remove(symbol_1)
+
+            col2, row2 = index_of_2
+            self.grid[col2].insert(row2, symbol_1)
+            self.grid[col2].remove(symbol_2)
+
+    # returns the location of the given symbol as a tuple.
+    #   if symbol is not found, returns (-1, -1)
+    def getIndexOf(self, symbol):
+        for col in range(0, self.num_cols):
+            for row in range(0, self.num_rows):
+                if self.grid[col][row] == symbol:
+                    return (col, row)
+        return (-1, -1)
+    
+
+        
 
 
 class Symbol:
     def __init__(self, color, symbol):
         self.color = color
-        self.symbol = symbol
-
-    #def __str__(self):
-    #   return self.symbol + " " + str(self.color)
-
-        
+        self.symbol = symbol        
