4d3198179089aefdafe59321f317b997998ed42c
Thomas Frick <thomas.m.frick@gmail.com>
2016-12-13 00:12:25 -0500

    high scores
    now savable!

--- high_score_manager.py
+++ high_score_manager.py
@@ -1,7 +1,10 @@
-high_scores = [0, 0, 0]
+import persistence 
+
+high_scores = persistence.load("data.json")
 def update_high_scores(score):
 	global high_scores
 	high_scores.append(score)
 	high_scores.remove(min(high_scores))
 	high_scores.sort()
 	high_scores.reverse()
+	persistence.store("data.json", high_scores)
--- persistence.py
+++ persistence.py
@@ -5,5 +5,5 @@ def store(file_name, data):
 		json.dump(data, out_file)
 		
 def load(file_name):
-	with open('data.txt', 'w') as in_file:
-		return json.load(data, in_file)
+	with open(file_name, 'r') as in_file:
+		return json.load(in_file)
